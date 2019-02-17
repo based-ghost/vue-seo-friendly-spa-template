e5b1cddeaaffe59e22b871e536a775ad3ceef7b2
Matt Areddia <mareddia@protonmail.com>
2019-02-17 14:49:03 -0600

    serviceWorker fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueAnalytics from 'vue-analytics';
-import './registerServiceWorker';
 import { googleConfig } from './config/google-analytics.config';
+import './registerServiceWorker';
 
 // Vue Configuration flags
 Vue.config.devtools = false;
