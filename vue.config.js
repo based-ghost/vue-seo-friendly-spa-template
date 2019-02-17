2516137cc11e12573be1baa7f38435d4b85f1fb2
Matt Areddia <mareddia@protonmail.com>
2019-02-17 11:25:14 -0600

    adding image-webpack-loader for image compression

--- vue.config.js
+++ vue.config.js
@@ -56,6 +56,21 @@ module.exports = {
     };
   },
 
+  chainWebpack: (config) => {
+    const imgRule = config.module.rule('images');
+    imgRule.use('file-loader')
+           .loader('image-webpack-loader')
+           .tap(options => {
+               const ret = options || {};
+               ret.bypassOnDebug = true;
+               ret.pngquant = {
+                   quality: '65-90',
+                   speed: 4
+               };
+        return ret;
+    })
+  },
+
   css: {
     loaderOptions: {
       sass: {
