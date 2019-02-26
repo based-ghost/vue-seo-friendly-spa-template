751a91c6878b6c22999b21d6f3e064c00451330f
Matt Areddia <mareddia@protonmail.com>
2019-02-26 12:19:27 -0600

    Fixing possibly last minor Lighthouse score infraction - LInks must have discernible text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="footer">
         <hr>
         <div class="buttons">
-            <a class="button is-medium">
+            <a class="button is-medium" aria-label="Twitter">
                 <span class="icon">
                     <font-awesome-icon :icon="['fab', 'twitter']"></font-awesome-icon>
                 </span>
@@ -10,6 +10,7 @@
             <a class="button is-medium"
                target="_blank"
                rel="noopener"
+               aria-label="GitHub"
                :href="parent.sfcData.GITHUB_URL">
                 <span class="icon">
                     <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
