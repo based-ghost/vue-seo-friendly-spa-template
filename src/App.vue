b1ce300719e5cba207e0a242e1a29b3455066f5f
Matt Areddia <mareddia@protonmail.com>
2019-03-02 16:14:14 -0600

    test

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default class App extends Vue {
     private showNavMenuOverlay: boolean = false;
     private mobileDeviceScrolled: boolean = false;
 
-    public created(): void {
+    public mounted(): void {
         this.checkForMobileDevice();
     }
 
