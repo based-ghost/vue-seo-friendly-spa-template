f7573991852ff980501de778dd45ba30cd97c660
Matt Areddia <mareddia@protonmail.com>
2019-03-02 11:39:13 -0600

    removing mount from main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,11 +20,20 @@ Vue.use(VueAnalytics, {
     }
 });
 
-// Mount app to Vue instance (mounted() has callback to fire event that the prerender plugin listens for)
 new Vue({
+    el: '#app-root',
     router,
     render: (h) => h(App),
-    mounted() {
-        document.dispatchEvent(new Event('render-event'));
-    },
-}).$mount('#app-root');
+    mounted () {
+      document.dispatchEvent(new Event('render-event'))
+    }
+});
+
+// Mount app to Vue instance (mounted() has callback to fire event that the prerender plugin listens for)
+// new Vue({
+//     router,
+//     render: (h) => h(App),
+//     mounted() {
+//         document.dispatchEvent(new Event('render-event'));
+//     },
+// }).$mount('#app-root');
